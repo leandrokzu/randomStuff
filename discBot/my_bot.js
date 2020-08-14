7d4a4132174165b5c8ff805ea54ac33bff54a50d
Leandro <leandroaugusto470@gmail.com>
2020-08-13 21:21:35 -0300

    implementado mais comandos e mais sons

--- discBot/my_bot.js
+++ discBot/my_bot.js
@@ -1,31 +1,32 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
+const prefix = "-";
+const token = "NzQzNDY0ODY3Njc1MjQyNTc2.XzVDnQ.DAAyql-4B160HSQbTFnXtjR3B2w"
+const commands = ['wow', 'ah', 'yamete', "an", "happy"];
 
 bot.on('message', message => {
-    if (message.content == '!Wow') {
-        var voiceChannel = message.member.voice.channel;
+    //para ignorar caso a msg seja do proprio bot
+    if (message.author.bot) return;
+    //para ignorar msg que não tenham o prefixo
+    if (!message.content.startsWith(prefix)) return;
+    //filtrando o comando para usa-lo no if
+    const command = message.content.split("-")[1];
+
+    var voiceChannel = message.member.voice.channel;
+    if (commands.includes(command)) {
+        if (!voiceChannel) {
+            return message.channel.send("Aye! Você tem que estar em um canal de voz para usar esse comando.")
+        }
         voiceChannel.join().then(connection => {
-
-            function play() {
-                if (Math.random() * 9 == 3) {
-                    connection.play('./wow.mp3');
-                    console.log("Aye!");
-                }else{
-                    console.log(";(");
-                }
-            }
-            setInterval(play, 1800000);
-
+            connection.play(`./${command}.mp3`);
         }).catch(err => console.log(err));
-    }
-});
 
-bot.on('message', message => {
-    if (message.content == '!notWow') {
-        var voiceChannel = message.member.voice.channel;
+    } else if (command == 'bye') {
         voiceChannel.leave();
+    } else {
+        return message.channel.send("Aye! Parece que este comando não existe, tente outra vez.")
     }
+
 });
 
-bot.login("NzQzNDY0ODY3Njc1MjQyNTc2.XzVDnQ.DAAyql-4B160HSQbTFnXtjR3B2w");
+bot.login(token);
