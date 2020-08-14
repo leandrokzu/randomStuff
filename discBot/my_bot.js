b8a8855e0c68f45af4733f1305a2b3ee7c7c01be
Leandro <leandroaugusto470@gmail.com>
2020-08-14 13:16:11 -0300

    mais soms e o comando help

--- discBot/my_bot.js
+++ discBot/my_bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const prefix = "-";
+const prefix = "!";
 const token = "NzQzNDY0ODY3Njc1MjQyNTc2.XzVDnQ.DAAyql-4B160HSQbTFnXtjR3B2w"
-const commands = ['wow', 'ah', 'yamete', "an", "happy"];
+const commands = ['wow', 'ah', 'yamete', "an", "happy", "ayaya", "begone", "oof", "rapaz", "ratinho", "uepa"];
 
 bot.on('message', message => {
     //para ignorar caso a msg seja do proprio bot
@@ -10,10 +10,23 @@ bot.on('message', message => {
     //para ignorar msg que não tenham o prefixo
     if (!message.content.startsWith(prefix)) return;
     //filtrando o comando para usa-lo no if
-    const command = message.content.split("-")[1];
+    const command = message.content.split("!")[1];
+    //criando mensagem de help
+    const helpMessage = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Comandos:')
+        .setDescription('Cada comando irá tocar algum som bem doido.')
+
+    commands.forEach(item => {
+        helpMessage.addField("!"+item, '\u200B');
+    });
+
+    bot.user.setActivity("Sua mãe pela janela");
 
     var voiceChannel = message.member.voice.channel;
-    if (commands.includes(command)) {
+    if (command == 'help') {
+        message.channel.send(helpMessage);
+    } else if (commands.includes(command)) {
         if (!voiceChannel) {
             return message.channel.send("Aye! Você tem que estar em um canal de voz para usar esse comando.")
         }
@@ -24,7 +37,7 @@ bot.on('message', message => {
     } else if (command == 'bye') {
         voiceChannel.leave();
     } else {
-        return message.channel.send("Aye! Parece que este comando não existe, tente outra vez.")
+        return message.channel.send("Aye! Parece que esse comando não existe, digite !help para ver os comandos disponíveis.")
     }
 
 });
